fb8560fcc738903e7115c82e233d724b4a8c683d
Alex Merkel <mail@alexmerkel.com>
2020-06-30 15:03:21 +0200

    Move urlfinder to own class

--- server.py
+++ server.py
@@ -7,13 +7,13 @@ import threading
 import sqlite3
 import math
 import mimetypes
-import re
 from datetime import datetime, timezone
 from dateutil.relativedelta import relativedelta
 from pycountry import languages
 from PIL import Image
 import flask
 import waitress
+from urlfinder import URLFinder
 
 
 __videosPerPage__ = 25
@@ -32,8 +32,8 @@ class Server(threading.Thread):
         '''
         #Call superclass init
         super(Server, self).__init__(*args, **kw)
-        #Define url finder from https://gist.github.com/gruber/8891611
-        self.urlfinder = re.compile("(?i)\\b((?:https?:(?:\\/{1,3}|[a-z0-9%])|[a-z0-9.\\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\\/)(?:[^\\s()<>{}\\[\\]]+|\\([^\\s()]*?\\([^\\s()]+\\)[^\\s()]*?\\)|\\([^\\s]+?\\))+(?:\\([^\\s()]*?\\([^\\s()]+\\)[^\\s()]*?\\)|\\([^\\s]+?\\)|[^\\s`!()\\[\\]{};:'\".,<>?\u00AB\u00BB\u201C\u201D\u2018\u2019])|(?:(?<!@)[a-z0-9]+(?:[.\\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\\b\\/?(?!@)))")
+        #Define url finder
+        self.urlfinder = URLFinder()
         #Setup database
         self.db = sqlite3.connect(dbPath, check_same_thread=False)
         self.db.row_factory = sqlite3.Row
@@ -179,7 +179,7 @@ class Server(threading.Thread):
         #Get mime type
         video["mimetype"] = mimetypes.guess_type(video["filepath"])[0]
         #Convert links in description
-        video["description"] = self.urlfinder.sub(r'<a href="\1">\1</a>', video["description"])
+        video["description"] = self.urlfinder.find(video["description"])
         #Render template
         return flask.render_template("watch.html", title=video["title"], video=video, nextVideo=nextVideo, previousVideo=previousVideo, latestVideo=latestVideo, channel=channel, base=self.baseinfo)
     # ########################################################################### #
@@ -214,7 +214,7 @@ class Server(threading.Thread):
                 v["timestamp"] = self.timestampToLocalTimeString(v["timestamp"])
             data["lastupdate"] = self.timestampToHumanString(data["lastupdate"])
             #Convert links in description
-            data["description"] = self.urlfinder.sub(r'<a href="\1">\1</a>', data["description"])
+            data["description"] = self.urlfinder.find(data["description"])
             #Render template
             return flask.render_template("channel-home.html", title=data["name"], info=data, videos=videos, base=self.baseinfo)
         #Get channel videos
@@ -257,7 +257,7 @@ class Server(threading.Thread):
                     links.append({"pretty": l[0], "url": l[1]})
                 data["links"] = links
             #Convert links in description
-            data["description"] = self.urlfinder.sub(r'<a href="\1">\1</a>', data["description"])
+            data["description"] = self.urlfinder.find(data["description"])
             #Render template
             return flask.render_template("channel-info.html", title=data["name"] + " - Info", info=data, base=self.baseinfo)
         #Unknown func, redirect to channel home
